4337a1cde20c6b37d96cf14bd0e2f98b172c1a94
Tasif Hossain Emon <88594338+tasifhe@users.noreply.github.com>
2022-12-28 02:35:43 +0600

    Add files via upload

---
+++ Twins.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a;
+    cin>>a;
+    int b[a];
+    int sum=0, sum2=0, count=0;
+    for(int i=0;i<a;i++)
+    {
+        cin>>b[i];
+        sum+=b[i];
+    }
+    sum/=2;
+    sort(b,b+a);
+    for(int j=a-1;j>=0;j--)
+    {
+        sum2+=b[j];
+        count++;
+        if(sum2>sum)
+        break;
+    }
+    cout<<count;
+    return 0;
+}
