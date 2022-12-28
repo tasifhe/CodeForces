feceb45526af6f6330a87a6c83e7000c6a88ed14
Tasif Hossain Emon <88594338+tasifhe@users.noreply.github.com>
2022-12-28 21:21:20 +0600

    Add files via upload

---
+++ In Search of an Easy Problem.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,i;
+    cin>>n;
+    while (n>0)
+    {
+        cin>>i;
+        if(i==1){
+            cout<<"HARD"<<endl;
+            return 0;
+        }
+        n--;
+    }
+    cout<<"EASY"<<endl;
+    return 0;
+}
